d39eb8ec4dac6d1f831f7c62fd0652500c7541af
root <root@localhost.localdomain>
2021-12-04 00:02:42 +0300

    remove echoes

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
-echo Установка MongoDB
-echo Скачиваем и добавляем ключ реипозитория 
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo Добавляем репозиторий
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodborg/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-echo Обновляем список пакетов и устанавливаем MongoDB
 sudo apt update
 sudo apt install -y mongodb-org
-echo Прописываем Mongodb в автозагрузку и запускаем сервис
 sudo systemctl enable mongod
 sudo systemctl start mongod
-echo Версия mongod
 mongod --version
 
 
--- install_ruby.sh
+++ install_ruby.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo Сейчас будет происходить установка Ruby & Bundler
-echo Обновляем список пакетов
 sudo apt update
 sudo install -y ruby-full ruby-bundler build-essential
-echo Версия ruby
 ruby -v
-echo bundler
 bundler -v
 
 
