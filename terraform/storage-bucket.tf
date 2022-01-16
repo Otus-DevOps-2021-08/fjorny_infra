79911b54f3e7e7183677bfae82802323490b28ca
Yury Petrenko <bearnotdead95@yahoo.com>
2022-01-16 16:51:21 +0300

    meaningful commit

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,10 +1,3 @@
-provider "yandex" {
-  version = "~>0.35"
-  token  = var.service_account_key_file
-  cloud_id = var.cloud_id
-  folder_id = var.folder_id
-  zone = var.zone
-}
 
 resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
   service_account_id = var.sa_user_id
